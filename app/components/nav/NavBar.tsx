6cbd216e0ef8d70d56f7eb9fd1686baaad1596f0
MostakemHossain <mostakememon123@gmail.com>
2024-02-27 10:54:12 +0600

    cart count show in ui

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,6 +1,7 @@
 import { Redressed } from "next/font/google";
 import Link from "next/link";
 import Container from "../Container";
+import CartCount from "./CartCount";
 
 const redressed= Redressed({ subsets: ['latin'],weight:['400'] })
 
@@ -13,7 +14,7 @@ const NavBar = () => {
                     <Link className={`${redressed.className} font-bold text-2xl`} href="/">E-Shop</Link>
                     <div className="hidden md:block">Search</div>
                     <div className="flex items-center gap-8 md:gap-12">
-                        <div>CartCount</div>
+                        <CartCount/>
                         <div>USer menu</div>
                     </div>
                 </div>
